c4060e47e4bcfc31e3bfe4146ce94f6575fa655b
Vedant Gupta <vedantgupta523@gmail.com>
2021-02-16 16:36:47 -0500

    repush

--- src/index.js
+++ src/index.js
@@ -33,17 +33,85 @@ const Text = () => {
   )
 }
 
-// sign up
-const Register = () => {
-  return (
-    <div>
-      <button class="start" onclick="document.getElementById('id01').style.display='block'">Get Started</button>
+class Modal extends React.Component {
+  constructor(props) {
+    super(props);
 
-    </div>    
-  )
+    this.state = {
+      showModal: false,
+      loading: false,
+      error: null
+    };
+  }
+
+  openModal() {
+    this.setState({
+      showModal: true
+    });
+  }
+
+  closeModal() {
+    this.setState({
+      showModal: false,
+      error: null
+    });
+  }
+
+  onLoginSuccess(method, response) {
+    console.log("logged successfully with " + method);
+  }
+
+  onLoginFail(method, response) {
+    console.log("logging failed with " + method);
+    this.setState({
+      error: response
+    });
+  }
+
+  startLoading() {
+    this.setState({
+      loading: true
+    });
+  }
+
+  finishLoading() {
+    this.setState({
+      loading: false
+    });
+  }
+
+  afterTabsChange() {
+    this.setState({
+      error: null
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <button class="Start" onClick={() => this.openModal()}>Open Modal</button>
+
+        <ReactModalLogin
+          visible={this.state.showModal}
+          onCloseModal={this.closeModal.bind(this)}
+          loading={this.state.loading}
+          error={this.state.error}
+          tabs={{
+            afterChange: this.afterTabsChange.bind(this)
+          }}
+          loginError={{
+            label: "Couldn't sign in, please try again."
+          }}
+          registerError={{
+            label: "Couldn't sign up, please try again."
+          }}
+          startLoading={this.startLoading.bind(this)}
+          finishLoading={this.finishLoading.bind(this)}
+        />
+      </div>
+    );
+  }
 }
-
-
 
 const App = () => {
   return (
@@ -51,8 +119,6 @@ const App = () => {
       <div style={{zIndex: 1, position: "absolute"}}><Background /></div>
       <div style={{zIndex: 2, position: "relative"}}><Header /></div>
       <div style={{zIndex: 2, position: "absolute"}}><Text /></div>
-
-      <div style={{zIndex: 2, position: "absolute"}}><Register /></div>
     </div>
   )
 }
