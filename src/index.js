99bfe4e22534519224913287fe8286b6c79e9be3
Vedant Gupta <vedantgupta523@gmail.com>
2021-02-16 16:20:48 -0500

    deleted files

--- src/index.js
+++ src/index.js
@@ -43,46 +43,7 @@ const Register = () => {
   )
 }
 
-class Sample extends React.Component {
-  constructor(props) {
-    super(props);
- 
-    this.state = {
-      showModal: false,
-      loading: false,
-      error: null
-    };
-  }
- 
-  openModal() {
-    this.setState({
-      showModal: true
-    });
-  }
- 
-  closeModal() {
-    this.setState({
-      showModal: false,
-      error: null
-    });
-  }
- 
-  onLoginSuccess(method, response) {
-    console.log("logged successfully with " + method);
-  }
- 
-  onLoginFail(method, response) {
-    console.log("logging failed with " + method);
-    this.setState({
-      error: response
-    });
-  }
- 
-  startLoading() {
-    this.setState({
-      loading: true
-    });
-  }
+
 
 const App = () => {
   return (
