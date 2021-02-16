3fdbc5cef3746179a264f8c026f84adc138e1b4f
Vedant Gupta <vedantgupta523@gmail.com>
2021-02-16 17:11:31 -0500

    modal

--- src/index.js
+++ src/index.js
@@ -3,8 +3,7 @@ import ReactDOM from 'react-dom';
 import background from "./img/background.jpg";
 import logo from "./img/folioLogo.png";
 import './index.css';
-
-import ReactModalLogin from "react-modal-login";
+import { Container } from './Container';
 
 // background image
 const Background = () => {
@@ -33,93 +32,20 @@ const Text = () => {
   )
 }
 
-class Modal extends React.Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      showModal: false,
-      loading: false,
-      error: null
-    };
-  }
-
-  openModal() {
-    this.setState({
-      showModal: true
-    });
-  }
-
-  closeModal() {
-    this.setState({
-      showModal: false,
-      error: null
-    });
-  }
-
-  onLoginSuccess(method, response) {
-    console.log("logged successfully with " + method);
-  }
-
-  onLoginFail(method, response) {
-    console.log("logging failed with " + method);
-    this.setState({
-      error: response
-    });
-  }
-
-  startLoading() {
-    this.setState({
-      loading: true
-    });
-  }
-
-  finishLoading() {
-    this.setState({
-      loading: false
-    });
-  }
-
-  afterTabsChange() {
-    this.setState({
-      error: null
-    });
-  }
-
-  render() {
-    return (
-      <div>
-        <button class="start" onClick={() => this.openModal()}>Get Started</button>
-
-        <ReactModalLogin
-          visible={this.state.showModal}
-          onCloseModal={this.closeModal.bind(this)}
-          loading={this.state.loading}
-          error={this.state.error}
-          tabs={{
-            afterChange: this.afterTabsChange.bind(this)
-          }}
-          loginError={{
-            label: "Couldn't sign in, please try again."
-          }}
-          registerError={{
-            label: "Couldn't sign up, please try again."
-          }}
-          startLoading={this.startLoading.bind(this)}
-          finishLoading={this.finishLoading.bind(this)}
-        />
-      </div>
-    );
-  }
-}
-
 const App = () => {
+  const triggerText = 'Get Started';
+    const onSubmit = (event) => {
+    event.preventDefault(event);
+    console.log(event.target.name.value);
+    console.log(event.target.email.value);
+  };
+
   return (
     <div>
       <div style={{zIndex: 1, position: "absolute"}}><Background /></div>
       <div style={{zIndex: 2, position: "relative"}}><Header /></div>
       <div style={{zIndex: 2, position: "absolute"}}><Text /></div>
-      <div style={{zIndex: 2, position: "absolute"}}><Modal /></div>
+      <div style={{zIndex: 2, position: "absolute"}}><Container triggerText={triggerText} onSubmit={onSubmit} /></div>
     </div>
   )
 }
