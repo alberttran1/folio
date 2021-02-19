2b4aa74751e34906b3d8ff0f5e17e8a0faa5f416
Vedant Gupta <vedantgupta523@gmail.com>
2021-02-18 20:17:50 -0500

    changes for husan

---
+++ src/Components/Start/Start.js
@@ -0,0 +1,11 @@
+import { Component } from 'react';
+
+export class Start extends Component{
+    render(){
+    return (
+      <button class="start">Get Started</button>
+    )
+  }
+}
+
+export default Start;
